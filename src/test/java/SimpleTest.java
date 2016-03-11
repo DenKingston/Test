038a7c4067d43a55ab16688574d708827737f568
Denis <denkingston@gmail.com>
2016-03-11 14:22:31 +0200

    1

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -13,16 +13,24 @@ public class SimpleTest {
         driver.manage().window().maximize();
         driver.get("https://accounts.google.com/ServiceLogin?service=mail&continue=https://mail.google.com/mail/&hl=ru#identifier");
 
-        WebElement addaccountButton = driver.findElement(By.cssSelector("#account-chooser-add-account"));
-        addaccountButton.click();
+     //   WebElement addaccountButton = driver.findElement(By.cssSelector("#account-chooser-add-account"));
+     //   addaccountButton.click();
 
         WebElement loginField = driver.findElement(By.cssSelector("#Email"));
         loginField.clear();
-        loginField.sendKeys("deny_kingston@gmail.com");
+        loginField.sendKeys("denykingston@gmail.com");
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
         WebElement nextButton = driver.findElement(By.cssSelector("#next"));
         nextButton.click();
+
+        WebElement passwordField = driver.findElement(By.cssSelector("#Passwd"));
+        passwordField.clear();
+        passwordField.sendKeys("p0lumorph");
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+        WebElement siginButton = driver.findElement(By.cssSelector("#signIn"));
+        siginButton.click();
 
     }
 }
