50f2ca80d0ddaa5eb22beb1e3d4d8de15e2f0edd
Denis <denkingston@gmail.com>
2016-03-16 20:11:45 +0200

    used PageObjectPattern

---
+++ src/test/java/google/login/page/LoginPage.java
@@ -0,0 +1,48 @@
+package google.login.page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage  {
+    private  WebDriver driver;
+    By userNameLocator = By.id ("Email");
+    By passwordLocator = By.id ("Passwd");
+    By signInButtonLocator = By.id("signIn");
+    By nextButtonLocator = By.id("next");
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void typeUsername() {
+        driver.findElement(userNameLocator).sendKeys("denykingston@gmail.com");
+    }
+
+    public void typePassword() {
+        driver.findElement(passwordLocator).sendKeys("p0lumorphp0lumorph");
+    }
+
+    public void typeNextButton() {
+        driver.findElement(nextButtonLocator).click();
+
+    }
+
+    public void clickSignInButton() {
+
+        driver.findElement(signInButtonLocator).click();
+    }
+
+   @FindBy(id = "error_block")
+    private WebElement errorText;
+
+   public boolean isError() {
+       if (errorText.isDisplayed()) {
+           return true;
+       } else {
+           return false;
+       }
+   }
+    
+}
---
+++ src/test/java/google/login/test/SimpleTest.java
@@ -0,0 +1,27 @@
+package google.login.test;
+
+import google.login.page.LoginPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class SimpleTest {
+    public static void main(String[] args) {
+
+    }
+    @Test
+    public void verifyLogin() {
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("https://accounts.google.com/ServiceLogin?service=mail&continue=https://mail.google.com/mail/&hl=ru#identifier");
+        LoginPage login = new LoginPage(driver);
+        login.typeUsername();
+        login.typeNextButton();
+        login.typePassword();
+        login.clickSignInButton();
+    }
+}
